488fbe0a8510dd04e0e968d35d87d81af1e7f76f
João Victor <138933172+fluidstar1338@users.noreply.github.com>
2025-05-30 13:03:31 -0300

    Add files via upload

---
+++ Arvore/exemplo_arvore/Exemplo_Arvore.java
@@ -0,0 +1,24 @@
+package exemplo_arvore;
+
+import java.util.Random;
+
+public class Exemplo_Arvore {
+
+    public static void main(String[] args) {
+        Random rand = new Random();
+        Arvore arvore = new Arvore();
+
+        for (int i = 0; i < 10; i++) {
+            arvore.insert(rand.nextInt(100), arvore.root);
+        }
+
+        System.out.println("\nPRE-ORDER");
+        arvore.preOrder(arvore.root);
+
+        System.out.println("\nIN-ORDER");
+        arvore.inOrder(arvore.root);
+
+        System.out.println("\nPOST-ORDER");
+        arvore.postOrder(arvore.root);
+    }
+}
